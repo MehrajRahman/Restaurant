57847cb3896d98c34c07f605049284f97286b2ad
MehrajRahman <rahmanmehraj627@gmail.com>
2021-09-01 16:19:39 +0600

    first commit

--- js/main.js
+++ js/main.js
@@ -17,7 +17,7 @@ let navBar = document.querySelectorAll('.nav-link');
 let navCollapse = document.querySelector ('.navbar-collapse.collapse');
 navBar.forEach( function(a) {
  a.addEventListener('click', function() {
-    navCollapse.classList.remove("show");
+    navCollapse.classList.add("show");
  }) })
 
 
